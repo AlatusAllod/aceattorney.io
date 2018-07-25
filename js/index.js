c8e2723b2b77c9f867d571a45c71e002ce1c0a30
AlatusAllod <37471090+AlatusAllod@users.noreply.github.com>
2018-07-25 13:27:24 +0400

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -18,10 +18,32 @@ $("nav ul li").on("click", function() {
 });
 
 $(document).ready(function(){
-	$(".header-menu nav").on("click","a", function (event) {
+	$("nav, .header-top").on("click","a", function (event) {
 	    event.preventDefault();
 	    var id  = $(this).attr('href'),
 	    top = $(id).offset().top;
-	    $('body,html').animate({scrollTop: top}, 1500);
+	    $('body,html').animate({scrollTop: top}, 1000);
+	});
+});
+
+$(document).ready(function() {
+	$('a.callback, .page-header button').click( function(event){
+		event.preventDefault();
+		$('#overlay').fadeIn(400,
+		 	function(){ 
+				$('#modal_form') 
+					.css('display', 'block')
+					.animate({opacity: 1, top: '50%'}, 200);
+		});
+	});
+	
+	$('#modal_close, #overlay').click( function(){
+		$('#modal_form')
+			.animate({opacity: 0, top: '45%'}, 200,
+				function(){
+					$(this).css('display', 'none');
+					$('#overlay').fadeOut(400);
+				}
+			);
 	});
 });
